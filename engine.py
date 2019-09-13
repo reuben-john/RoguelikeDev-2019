d43c2db898b3f466af0d28139a00403bfb14d57f
Reuben John <rjkoppy@gmail.com>
2019-09-13 14:33:37 -0500

    Set up console

--- engine.py
+++ engine.py
@@ -2,7 +2,34 @@ import tcod as libtcod
 
 
 def main():
-    print('Hello World')
+    """Game setup and loop"""
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+    libtcod.console_set_custom_font(
+        'arial10x10.png', libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    libtcod.console_init_root(
+        screen_width, screen_height, 'Roguelike 2019', False)
+
+    # Main game loop
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+        libtcod.console_set_default_foreground(0, libtcod.white)
+        libtcod.console_put_char(
+            0, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_flush()
+
+        key = libtcod.console_check_for_keypress()
+
+        if key.vk == libtcod.KEY_ESCAPE:
+            return True
 
 
 if __name__ == '__main__':

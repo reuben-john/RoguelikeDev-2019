569c359be37b6de0b743a9a7b2e6534f54615948
Reuben John <rjkoppy@gmail.com>
2019-09-13 17:02:55 -0500

    Create two empty rooms

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,3 +1,4 @@
+from map_objects.rectangle import Rect
 from map_objects.tile import Tile
 
 
@@ -14,6 +15,21 @@ class GameMap:
                  for x in range(self.width)]
 
         return tiles
+
+    def make_map(self):
+        # Create two rooms for demonstration purposes
+        room1 = Rect(20, 15, 10, 15)
+        room2 = Rect(35, 15, 10, 15)
+
+        self.create_room(room1)
+        self.create_room(room2)
+
+    def create_room(self, room):
+        # go through tiles in rectangle and make them passable
+        for x in range(room.x1 + 1, room.x2):
+            for y in range(room.y1 + 1, room.y2):
+                self.tiles[x][y].blocked = False
+                self.tiles[x][y].block_sight = False
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:

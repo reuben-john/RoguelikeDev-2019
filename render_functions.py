fd6c1d42df6669faa2717d877cc74f8950628b6e
Reuben John <rjkoppy@gmail.com>
2019-09-13 21:55:42 -0500

    Hide map and entities

--- render_functions.py
+++ render_functions.py
@@ -16,7 +16,9 @@ def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, sc
                     else:
                         libtcod.console_set_char_background(
                             con, x, y, colors.get('light_ground'), libtcod.BKGND_SET)
-                else:
+
+                        game_map.tiles[x][y].explored = True
+                elif game_map.tiles[x][y].explored:
                     if wall:
                         libtcod.console_set_char_background(
                             con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
@@ -25,7 +27,7 @@ def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, sc
                             con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
 
     for entity in entities:
-        draw_entity(con, entity)
+        draw_entity(con, entity, fov_map)
 
     libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
 
@@ -35,10 +37,11 @@ def clear_all(con, entities):
         clear_entity(con, entity)
 
 
-def draw_entity(con, entity):
-    libtcod.console_set_default_foreground(con, entity.color)
-    libtcod.console_put_char(con, entity.x, entity.y,
-                             entity.char, libtcod.BKGND_NONE)
+def draw_entity(con, entity, fov_map):
+    if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+        libtcod.console_set_default_foreground(con, entity.color)
+        libtcod.console_put_char(con, entity.x, entity.y,
+                                 entity.char, libtcod.BKGND_NONE)
 
 
 def clear_entity(con, entity):

68fce1e476f3f743d30cd433a2d23beef0c94e3a
Reuben John <rjkoppy@gmail.com>
2019-09-14 14:57:41 -0500

    Add game state management

--- engine.py
+++ engine.py
@@ -1,7 +1,8 @@
 import tcod as libtcod
 
-from entity import Entity
+from entity import Entity, get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
+from game_states import GameStates
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
 from render_functions import clear_all, render_all
@@ -29,7 +30,7 @@ def main():
               'light_wall': libtcod.Color(130, 110, 50),
               'light_ground': libtcod.Color(200, 180, 50)}
 
-    player = Entity(0, 0, '@', libtcod.white)
+    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True)
     entities = [player]
 
     libtcod.console_set_custom_font(
@@ -50,6 +51,8 @@ def main():
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
+
+    game_state = GameStates.PLAYERS_TURN
 
     # Main game loop
     while not libtcod.console_is_window_closed():
@@ -72,18 +75,37 @@ def main():
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
-        if move:
+        if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
-            if not game_map.is_blocked(player.x + dx, player.y + dy):
-                player.move(dx, dy)
+            destination_x = player.x + dx
+            destination_y = player.y + dy
 
-                fov_recompute = True
+            if not game_map.is_blocked(destination_x, destination_y):
+                target = get_blocking_entities_at_location(
+                    entities, destination_x, destination_y)
+
+                if target:
+                    print('You kick the ' + target.name +
+                          ' in the shins, much to its annoyance!')
+                else:
+                    player.move(dx, dy)
+
+                    fov_recompute = True
+
+            game_state = GameStates.ENEMY_TURN
 
         if exit:
             return True
 
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+        if game_state == GameStates.ENEMY_TURN:
+            for entity in entities:
+                if entity != player:
+                    print('The ' + entity.name + ' ponders what to do.')
+
+            game_state = GameStates.PLAYERS_TURN
 
 
 if __name__ == '__main__':

a45e2eb5a16e9b8e17dac3c3677cb6c51165fa4d
Reuben John <rjkoppy@gmail.com>
2019-09-14 20:41:41 -0500

    Add health bar

--- engine.py
+++ engine.py
@@ -66,7 +66,7 @@ def main():
             recompute_fov(fov_map, player.x, player.y, fov_radius,
                           fov_light_walls, fov_algorithm)
 
-        render_all(con, entities, game_map, fov_map, fov_recompute,
+        render_all(con, entities, player, game_map, fov_map, fov_recompute,
                    screen_width, screen_height, colors)
 
         libtcod.console_flush()

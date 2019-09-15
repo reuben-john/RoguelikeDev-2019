0e59d0303ceefc1b316899b58480a73570c711a3
Reuben John <rjkoppy@gmail.com>
2019-09-15 09:29:56 -0500

    Add missing y to fix message log

--- render_functions.py
+++ render_functions.py
@@ -72,7 +72,8 @@ def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, m
     for message in message_log.messages:
         libtcod.console_set_default_foreground(panel, message.color)
         libtcod.console_print_ex(
-            panel, message_log.x, libtcod.BKGND_NONE, libtcod.LEFT, message.text)
+            panel, message_log.x, y, libtcod.BKGND_NONE, libtcod.LEFT, message.text)
+        y += 1
 
     render_bar(panel, 1, 1, bar_width, 'HP', player.fighter.hp,
                player.fighter.max_hp, libtcod.light_red, libtcod.darker_red)
